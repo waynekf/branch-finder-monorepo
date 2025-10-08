388746f8e1205bc94f32f1142d826126ceaa7b20
Wayne King-Farlow <wayne.king-farlow@skipton.co.uk>
2025-10-08 14:05:46 +0100

    Added redirect to home page

--- branch-finder-monorepo/apps/branch-finder-website/next.config.js
+++ branch-finder-monorepo/apps/branch-finder-website/next.config.js
@@ -10,6 +10,15 @@ const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 const plugins = [
